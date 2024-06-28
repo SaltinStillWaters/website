0fef662210768daa2c52794d21be588600db7a69
SaltinStillWaters <SaltinStillWaters@gmail.com>
2024-06-28 15:02:01 +0800

    added promotion

--- frontend/welcome.php
+++ frontend/welcome.php
@@ -3,20 +3,29 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="welcomeSheet.css">
+    
+    <link rel="stylesheet" href="general.css">
+    <link rel="stylesheet" href="slideshow.css">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
+
     <title>Mobile Legends</title>
 </head>
 <body>
-    <div class="banner">
-        <div class="navbar">
-            <img src="../resources/ml_logo.png" class="logo">
-            <ul>
-                <li><a href="#">Home</a></li>
-                <li><a href="#">Strategy Guides</a></li>
-                <li><a href="#">Hero Rankings</a></li>
-                <li><a href="#">Counter Picking</a></li>
-            </ul>
-        </div>
+<div class="container">
+    
+    <div class="navbar">
+        <img src="../resources/ml_logo.png" class="logo">
+        <ul>
+            <li><a href="#">Home</a></li>
+            <li><a href="#">Strategy Guides</a></li>
+            <li><a href="#">Hero Rankings</a></li>
+            <li><a href="#">Counter Picking</a></li>
+        </ul>
+    </div>
+
+    <div class="card bg-transparent mx-auto" style="width: 720px; height: 450px">
+        <h5 class="card-header text-center text-light fw-bold fst-italic">LATEST NEWS</h5>
         <div class="slider-frame">
             <div class="slide-images">
                 <img src="../resources/welcome/news/proQ.jpg" alt="Slide 1">
@@ -28,5 +37,28 @@
         </div>
     </div>
 
+    
+    <h5 class="text-center text-light fw-bold fst-italic fs-1">JOIN THE BATTLE!</h5><br>
+    <p class="text-light lh-base text-center">
+        Step into the world of Mobile Legends: Bang Bang, where epic battles and legendary heroes await!
+    </p>
+    <h5 class="text-center text-light fw-bold fst-italic fs-1">Diverse Characters and Rich Backstories</h5><br>
+    <p class="text-light lh-base text-center">
+        Immerse yourself in a universe teeming with diverse characters, each with unique abilities and rich backstories that span realms and ages. From the noble knights of the Moniyan Empire to the mystical warriors of the Land of Dawn, every hero has a tale of courage, sacrifice, and triumph.
+    </p>
+    <h5 class="text-center text-light fw-bold fst-italic fs-1">Thrilling 5v5 Battles</h5><br>
+    <p class="text-light lh-base text-center">
+        Join forces with friends in thrilling 5v5 battles, strategize to outmaneuver your opponents, and experience the adrenaline rush of fast-paced combat.
+    </p>
+    <h5 class="text-center text-light fw-bold fst-italic fs-1">Thrilling 5v5 Battles</h5><br>
+    <p class="text-light lh-base text-center">
+        Whether you're a seasoned warrior or a newcomer to the battlefield, Mobile Legends promises an adventure that will ignite your passion for victory and camaraderie.
+    </p>
+    <h5 class="text-center text-light fw-bold fst-italic fs-1">The Battle Awaits</h5><br>
+    <p class="text-light lh-base text-center">
+        Are you ready to carve your name into legend? The battle awaits!
+    </p>
+        
+</div>
 </body>
 </html>
