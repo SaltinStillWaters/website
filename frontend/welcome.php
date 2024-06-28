4322e759c223977897b0b0d673f4331ce79195ab
SaltinStillWaters <SaltinStillWaters@gmail.com>
2024-06-28 18:19:41 +0800

    finisged content for welcome page

--- frontend/welcome.php
+++ frontend/welcome.php
@@ -12,53 +12,73 @@
     <title>Mobile Legends</title>
 </head>
 <body>
-<div class="container">
+<div class="">
     
-    <div class="navbar">
-        <img src="../resources/ml_logo.png" class="logo">
-        <ul>
-            <li><a href="#">Home</a></li>
-            <li><a href="#">Strategy Guides</a></li>
-            <li><a href="#">Hero Rankings</a></li>
-            <li><a href="#">Counter Picking</a></li>
-        </ul>
-    </div>
-
-    <div class="card bg-transparent mx-auto" style="width: 720px; height: 450px">
-        <h5 class="card-header text-center text-light fw-bold fst-italic">LATEST NEWS</h5>
-        <div class="slider-frame">
-            <div class="slide-images">
-                <img src="../resources/welcome/news/proQ.jpg" alt="Slide 1">
-                <img src="../resources/welcome/news/zhuxin.jpg" alt="Slide 2">
-                <img src="../resources/welcome/news/starlight.jpg" alt="Slide 3">
-                <img src="../resources/welcome/news/recharge.jpg" alt="Slide 4">
-                <img src="../resources/welcome/news/proQ.jpg" alt="Slide 1">
+    <div class="group-1">
+        <div class="navbar">
+            <img src="../resources/ml_logo.png" class="logo">
+            <ul>
+                <li><a href="#">Home</a></li>
+                <li><a href="#">Strategy Guides</a></li>
+                <li><a href="#">Hero Rankings</a></li>
+                <li><a href="#">Counter Picking</a></li>
+            </ul>
+        </div>
+        
+        <div class="card bg-transparent mx-auto" style="width: 900px; height: 550px">
+            <h5 class="card-header text-center text-light fw-bold fst-italic">LATEST NEWS</h5>
+            <div class="slider-frame">
+                <div class="slide-images">
+                    <img src="../resources/welcome/news/proQ.jpg" alt="Slide 1">
+                    <img src="../resources/welcome/news/zhuxin.jpg" alt="Slide 2">
+                    <img src="../resources/welcome/news/starlight.jpg" alt="Slide 3">
+                    <img src="../resources/welcome/news/recharge.jpg" alt="Slide 4">
+                    <img src="../resources/welcome/news/proQ.jpg" alt="Slide 1">
+                </div>
             </div>
         </div>
     </div>
-
     
-    <h5 class="text-center text-light fw-bold fst-italic fs-1">JOIN THE BATTLE!</h5><br>
-    <p class="text-light lh-base text-center">
-        Step into the world of Mobile Legends: Bang Bang, where epic battles and legendary heroes await!
-    </p>
-    <h5 class="text-center text-light fw-bold fst-italic fs-1">Diverse Characters and Rich Backstories</h5><br>
-    <p class="text-light lh-base text-center">
-        Immerse yourself in a universe teeming with diverse characters, each with unique abilities and rich backstories that span realms and ages. From the noble knights of the Moniyan Empire to the mystical warriors of the Land of Dawn, every hero has a tale of courage, sacrifice, and triumph.
-    </p>
-    <h5 class="text-center text-light fw-bold fst-italic fs-1">Thrilling 5v5 Battles</h5><br>
-    <p class="text-light lh-base text-center">
-        Join forces with friends in thrilling 5v5 battles, strategize to outmaneuver your opponents, and experience the adrenaline rush of fast-paced combat.
-    </p>
-    <h5 class="text-center text-light fw-bold fst-italic fs-1">A Game for All Players</h5><br>
-    <p class="text-light lh-base text-center">
-        Whether you're a seasoned warrior or a newcomer to the battlefield, Mobile Legends promises an adventure that will ignite your passion for victory and camaraderie.
-    </p>
-    <h5 class="text-center text-light fw-bold fst-italic fs-1">The Battle Awaits</h5><br>
-    <p class="text-light lh-base text-center">
-        Are you ready to carve your name into legend? The battle awaits!
-    </p>
+    <div class="group-2">
+        <h5 class="text-center text-dark fw-bold fst-italic fs-1">JOIN THE BATTLE!</h5><br>
+        <p class="text-dark lh-base text-center fs-4 fw-bold">
+            Step into the world of Mobile Legends: Bang Bang, where epic battles and legendary heroes await!
+        </p>
+    </div>
+
+    <div class="group-3">
+        <div class="card bg-transparent mx-auto" style="width: 35%">
+            <h5 class="card-header text-center text-light fw-bold fst-italic">Diverse Characters and Rich Backstories</h5>
+            <p class="card-text text-light lh-base text-center">
+            Immerse yourself in a universe teeming with diverse characters, each with unique abilities and rich backstories that span realms and ages. 
+            <br>
+            From the noble knights of the Moniyan Empire to the mystical warriors of the Land of Dawn, every hero has a tale of courage, sacrifice, and triumph.
+        </p>
+        </div>
+
+        <div class="card bg-transparent mx-auto" style="width: 27%">
+            <h5 class="card-header text-center text-light fw-bold fst-italic">Thrilling 5v5 Battles</h5>
+            <p class="card-text text-light lh-base text-center">
+            Join forces with friends in thrilling 5v5 battles, strategize to outmaneuver your opponents, and experience the adrenaline rush of fast-paced combat.
+        </p>
+        </div>
+
+        <div class="card bg-transparent mx-auto" style="width: 30%">
+            <h5 class="card-header text-center text-light fw-bold fst-italic">A Game for All Players</h5>
+            <p class="card-text text-light lh-base text-center">
+            Whether you're a seasoned warrior or a newcomer to the battlefield, Mobile Legends promises an adventure that will ignite your passion for victory and camaraderie.
+        </p>
+        </div>
         
+        
+        <h5 class="text-center text-light fw-bold fst-italic fs-1">The Battle Awaits!</h5><br>
+    </div>
+    
+    <div class="promotion">
+        <img src="../resources/app_store.png" alt="">
+        <img src="../resources/google_play.png" alt="">
+        <img src="../resources/ml_logo.png" alt="" class="last">
+    </div>
 </div>
 </body>
 </html>
