edf9ccada0d2ec04cd8fac02818804896c189a5d
SaltinStillWaters <SaltinStillWaters@gmail.com>
2024-07-06 13:55:27 +0800

    fix

--- index.php
+++ index.php
@@ -2,31 +2,47 @@
     <link rel="stylesheet" href="css/base/base.css">
     <link rel="stylesheet" href="css/layout/header.css">
     <link rel="stylesheet" href="css/features/slideshow.css">
+    <link rel="stylesheet" href="css/layout/footer.css">
     
 </head>
 
 <body>
-    <header>
-        <a href="#" class="logo">ml companion</a>
-        <ul>
-            <li><a href="#">Strategy Guides</a></li>
-            <li><a href="#">Hero Rankings</a></li>
-            <li><a href="#">Counter Picking</a></li>
-            <div class="logout">                
-                <li><a href="logout.php">Log out</a></li>
-            </div>
-        </ul>
-    </header>
+    <div class="blk_bg">
 
-    <div class="slider-frame">
-        <div class="slide-images">
-            <img src="resources/welcome/news/proQ.jpg" alt="Slide 1">
-            <img src="resources/welcome/news/zhuxin.jpg" alt="Slide 2">
-            <img src="resources/welcome/news/starlight.jpg" alt="Slide 3">
-            <img src="resources/welcome/news/recharge.jpg" alt="Slide 4">
-            <img src="resources/welcome/news/proQ.jpg" alt="Slide 1">
+        <header>
+            <a href="#" class="logo">ml companion</a>
+            <ul>
+                <li><a href="#">Strategy Guides</a></li>
+                <li><a href="#">Hero Rankings</a></li>
+                <li><a href="#">Counter Picking</a></li>
+                <div class="logout">                
+                    <li><a href="logout.php">Log out</a></li>
+                </div>
+            </ul>
+        </header>
+        
+        <div class="slideshow">
+            <div class="title">Latest News</div>
+
+            <div class="slider-frame">
+                <div class="slide-images">
+                    <img src="resources/welcome/news/proQ.jpg" alt="Slide 1">
+                    <img src="resources/welcome/news/zhuxin.jpg" alt="Slide 2">
+                    <img src="resources/welcome/news/starlight.jpg" alt="Slide 3">
+                    <img src="resources/welcome/news/recharge.jpg" alt="Slide 4">
+                    <img src="resources/welcome/news/proQ.jpg" alt="Slide 1">
+                </div>
+            </div>
         </div>
+
     </div>
+
+
+    <footer>
+        <img src="resources/app_store.png" alt="">
+        <img src="resources/google_play.png" alt="">
+        <img src="resources/ml_logo.png" alt="" class="logo">
+    </footer>
 
     <script type="text/javascript">
         window.addEventListener("scroll", function() {
