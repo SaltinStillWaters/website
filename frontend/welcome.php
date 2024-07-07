07d097b452d0fea0180499e80730b00ae38f0953
SaltinStillWaters <SaltinStillWaters@gmail.com>
2024-07-07 10:04:29 +0800

    transfered welcome html to welcome.php

--- frontend/welcome.php
+++ frontend/welcome.php
@@ -1,33 +1,30 @@
-<!DOCTYPE html>
-<html lang="en">
 <head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="../css/base/base.css">
+    <link rel="stylesheet" href="../css/layout/header.css">
+    <link rel="stylesheet" href="../css/features/slideshow.css">
+    <link rel="stylesheet" href="../css/pages/welcome.css">
+    <link rel="stylesheet" href="../css/layout/footer.css">
     
-    <link rel="stylesheet" href="general.css">
-    <link rel="stylesheet" href="slideshow.css">
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
-
-    <title>Mobile Legends</title>
 </head>
+
 <body>
-<div class="">
-    
-    <div class="group-1">
-        <div class="navbar">
-            <img src="../resources/ml_logo.png" class="logo">
+    <div class="blk_bg">
+
+        <header>
+            <a href="#" class="logo">ml companion</a>
             <ul>
-                <li><a href="#">Home</a></li>
                 <li><a href="#">Strategy Guides</a></li>
                 <li><a href="#">Hero Rankings</a></li>
                 <li><a href="#">Counter Picking</a></li>
-                <li><a href="logout.php">Log out</a></li>
+                <div class="logout">                
+                    <li><a href="logout.php">Log out</a></li>
+                </div>
             </ul>
-        </div>
+        </header>
         
-        <div class="card bg-transparent mx-auto" style="width: 900px; height: 550px">
-            <h5 class="card-header text-center text-light fw-bold fst-italic">LATEST NEWS</h5>
+        <div class="slideshow">
+            <div class="title">Latest News</div>
+
             <div class="slider-frame">
                 <div class="slide-images">
                     <img src="../resources/welcome/news/proQ.jpg" alt="Slide 1">
@@ -38,48 +35,47 @@
                 </div>
             </div>
         </div>
+
+    </div>
+
+    <div class="story_text">
+        Experience the Thrill of Battle
+    </div>
+    <div class="aulus">
+    </div>
+
+    <div class="story_text">
+        Protect your Allies
+    </div>
+    <div class="stun">
+    </div>
+
+    <div class="story_text">
+        Exterminate Demons
+    </div>
+    <div class="monster">
+    </div>
+
+    <div class="story_text">
+        Purify the World
+    </div>
+    <div class="kadita">
+    </div>
+
+    <div class="story_text">
+        Your Epic Journey Awaits...
     </div>
     
-    <div class="group-2">
-        <h5 class="text-center text-dark fw-bold fst-italic fs-1">JOIN THE BATTLE!</h5><br>
-        <p class="text-dark lh-base text-center fs-4 fw-bold">
-            Step into the world of Mobile Legends: Bang Bang, where epic battles and legendary heroes await!
-        </p>
-    </div>
-
-    <div class="group-3">
-        <div class="card bg-transparent mx-auto" style="width: 35%">
-            <h5 class="card-header text-center text-light fw-bold fst-italic">Diverse Characters and Rich Backstories</h5>
-            <p class="card-text text-light lh-base text-center">
-            Immerse yourself in a universe teeming with diverse characters, each with unique abilities and rich backstories that span realms and ages. 
-            <br>
-            From the noble knights of the Moniyan Empire to the mystical warriors of the Land of Dawn, every hero has a tale of courage, sacrifice, and triumph.
-        </p>
-        </div>
-
-        <div class="card bg-transparent mx-auto" style="width: 27%">
-            <h5 class="card-header text-center text-light fw-bold fst-italic">Thrilling 5v5 Battles</h5>
-            <p class="card-text text-light lh-base text-center">
-            Join forces with friends in thrilling 5v5 battles, strategize to outmaneuver your opponents, and experience the adrenaline rush of fast-paced combat.
-        </p>
-        </div>
-
-        <div class="card bg-transparent mx-auto" style="width: 30%">
-            <h5 class="card-header text-center text-light fw-bold fst-italic">A Game for All Players</h5>
-            <p class="card-text text-light lh-base text-center">
-            Whether you're a seasoned warrior or a newcomer to the battlefield, Mobile Legends promises an adventure that will ignite your passion for victory and camaraderie.
-        </p>
-        </div>
-        
-        
-        <h5 class="text-center text-light fw-bold fst-italic fs-1">The Battle Awaits!</h5><br>
-    </div>
-    
-    <div class="promotion">
+    <footer>
         <img src="../resources/app_store.png" alt="">
         <img src="../resources/google_play.png" alt="">
-        <img src="../resources/ml_logo.png" alt="" class="last">
-    </div>
-</div>
+        <img src="../resources/ml_logo.png" alt="" class="logo">
+    </footer>
+
+    <script type="text/javascript">
+        window.addEventListener("scroll", function() {
+            var header = document.querySelector("header");
+            header.classList.toggle("sticky", window.scrollY > 0);
+        });
+    </script>
 </body>
-</html>
