cd01030ef7d45c3295a7b26588b7905d992ba672
SaltinStillWaters <SaltinStillWaters@gmail.com>
2024-07-17 21:07:04 +0800

    added error messages for welcome page

--- pages/adminWelcome.php
+++ pages/adminWelcome.php
@@ -5,21 +5,16 @@ require_once('../backend/file.php');
 require_once('../backend/page_controller.php');
 
 PageController::init(false);
-
-if ($_SERVER['REQUEST_METHOD'] === 'POST')
-{
-    if (isset($_POST['remove']))
-    {
+if ($_SERVER['REQUEST_METHOD'] === 'POST') {
+    if (isset($_POST['remove'])) {
         $_SESSION['admin'] = array_diff($_SESSION['admin'], [$_POST['remove']]);
     }
 
-    if (isset($_POST['fileUploaded']))
-    {
+    if (isset($_POST['fileUploaded'])) {
         $canUpload = 1;
 
-        if (count($_SESSION['admin']) == 4)
-        {
-            echo 'News must have exactly 4 images';
+        if (count($_SESSION['admin']) == 4) {
+            $error = 'News must have exactly 4 images';
             $canUpload = 0;
         }
 
@@ -27,59 +22,50 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST')
         $targetFile = $targetDir . basename($_FILES['upload']['name']);
         $fileType = strtolower(pathinfo($targetFile, PATHINFO_EXTENSION));
 
-        if ($canUpload && file_exists($targetFile)) 
-        {
-            if (!in_array(basename($_FILES['upload']['name']), $_SESSION['admin']))
-            {
+        if ($canUpload && file_exists($targetFile)) {
+            if (!in_array(basename($_FILES['upload']['name']), $_SESSION['admin'])) {
                 $_SESSION['admin'][] = basename($_FILES['upload']['name']);
             }
 
             $canUpload = 0;
         }
 
-        if ($_FILES["upload"]["size"] > 1000000) 
-        {
-            echo "File is too large. 1MB is the max";
+        if ($_FILES["upload"]["size"] > 1000000) {
+            $error = "File is too large. 1MB is the max";
             $canUpload = 0;
         }
 
         if ($fileType != "jpg" && $fileType != "png" && $fileType != "jpeg" && $fileType != "gif") {
-            echo "Only JPG, JPEG, PNG & GIF files are allowed.";
+            $error = "Only JPG, JPEG, PNG & GIF files are allowed.";
             $canUpload = 0;
         }
-        
+
         if ($canUpload && move_uploaded_file($_FILES["upload"]["tmp_name"], $targetFile)) {
-            echo "File " . htmlspecialchars(basename($_FILES["upload"]["name"])) . " has been uploaded.";
+            $message = "File " . htmlspecialchars(basename($_FILES["upload"]["name"])) . " has been uploaded.";
             $_SESSION['admin'][] = basename($_FILES['upload']['name']);
         }
     }
 
-    if (isset($_POST['save']))
-    {
+    if (isset($_POST['save'])) {
         $canSave = 1;
 
-        if (count($_SESSION['admin']) !== 4)
-        {
-            echo 'News must have exactly 4 images';
+        if (count($_SESSION['admin']) !== 4) {
+            $error = 'News must have exactly 4 images';
             $canSave = 0;
         }
 
-        if ($canSave)
-        {
+        if ($canSave) {
             $toRemove = array_diff(scandir('../resources/welcome/newsTemp'), $_SESSION['admin'], array('..', '.'));
-            foreach ($toRemove as $file)
-            {
-                echo $file;
+            foreach ($toRemove as $file) {
                 unlink('../resources/welcome/newsTemp/' . $file);
             }
             Files::copyFolder('../resources/welcome/newsTemp', '../resources/welcome/news');
 
-            echo 'changes have been saved';
+            $message = 'changes have been saved';
         }
     }
 
-    if (isset($_POST['discard']))
-    {
+    if (isset($_POST['discard'])) {
         header('Location: transition/toAdminWelcome.php');
         exit();
     }
@@ -91,6 +77,7 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST')
 
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
@@ -101,58 +88,70 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST')
     <link rel="stylesheet" href="../css/pages/welcome.css">
     <link href="../css/pages/adminWelcome.css" rel="stylesheet">
 </head>
+
 <body>
-<header>
-            <a href="#" class="logo">ml companion</a>
-            <ul>
-                <li><a href="transition/toAdminWelcome">Strategy Guides</a></li>
-                <li><a href="transition/toAdminRankings.php">Hero Rankings</a></li>
-                <li><a href='transition/toAdminForum.php'>Forums</a></li>
-                <div class="logout">                
-                    <li><a href="logout.php">Log out</a></li>
-                </div>
-            </ul>
-        </header>
+    <header>
+        <a href="#" class="logo">ml companion</a>
+        <ul>
+            <li><a href="transition/toAdminWelcome.php">Strategy Guides</a></li>
+            <li><a href="transition/toAdminRankings.php">Hero Rankings</a></li>
+            <li><a href='adminForum.php'>Forums</a></li>
+            <div class="logout">
+                <li><a href="logout.php">Log out</a></li>
+            </div>
+        </ul>
+    </header>
 
     <form id='form' method="post" enctype="multipart/form-data">
-    <?php
-    
-    $ctr = 0;
-    foreach($_SESSION['admin'] as $file)
-    {
-        if ($ctr === 0 || $ctr === 2)
-        {
-            echo "<div class='img-container'>";
+        <?php
+
+        $ctr = 0;
+        foreach ($_SESSION['admin'] as $file) {
+            if ($ctr === 0 || $ctr === 2) {
+                echo "<div class='img-container'>";
+            }
+
+            echo "<img src='../resources/welcome/newsTemp/" . $file . "'>";
+            echo "<button type='submit' class='remove' name='remove' value='$file'>x</button>";
+
+            if ($ctr === 1 || $ctr === 3) {
+                echo "</div>";
+            }
+            $ctr++;
         }
+        echo "</div>";
 
-        echo "<img src='../resources/welcome/newsTemp/" . $file . "'>";
-        echo "<button type='submit' class='remove' name='remove' value='$file'>x</button>";
-
-        if ($ctr === 1 || $ctr === 3)
-        {
-            echo "</div>";
-        }
-        $ctr++;
-    }
-    echo "</div>";
-
-    echo "<input type='file' class='upload' name='upload' value='Upload' onchange=fileUploaded()>";
-    echo "<input type='submit' class='save' name='save' value='Save'>";
-    echo "<input type='submit' class='discard' name='discard' value='Discard'>";
-    ?>
+        echo "<input type='file' class='upload' name='upload' value='Upload' onchange=fileUploaded()>";
+        echo "<input type='submit' class='save' name='save' value='Save'>";
+        echo "<input type='submit' class='discard' name='discard' value='Discard'>";
+        ?>
     </form>
-
-<script>
-    function fileUploaded() {
-        var form = document.getElementById('form');
-        var hiddenInput = document.createElement("input");
+    <?php
+            if (isset($error))
+            {                
+                echo "<div class='error'>";
+                echo "<p>$error</p>";
+                echo "</div>";
+            }
+            if (isset($message))
+            {
+                echo "<div class='message'>";
+                echo "<p>$message</p>";
+                echo "</div>";
+            }
+        ?>
+    <script>
+        function fileUploaded() {
+            var form = document.getElementById('form');
+            var hiddenInput = document.createElement("input");
             hiddenInput.type = "hidden";
             hiddenInput.name = "fileUploaded";
             hiddenInput.value = "true";
 
-        form.appendChild(hiddenInput);
-        form.submit();
-    }        
-</script>
+            form.appendChild(hiddenInput);
+            form.submit();
+        }
+    </script>
 </body>
+
 </html>
